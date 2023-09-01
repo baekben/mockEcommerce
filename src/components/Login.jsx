add1af02c40ca5de1916346770672704c0abcb1f
Ben <ben10baek@hotmail.com>
2023-08-31 19:33:04 -0700

    Set up divs for login

---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+export default function Login() {
+  return (
+    <>
+      <div className="loginContainer">
+        <div className="loginBox">
+          <div className="loginTitle">
+            <h2></h2>
+          </div>
+          <div className="loginArea"></div>
+        </div>
+        <div className="signUpBox">
+          <div className="signUpTitle"></div>
+          <div className="joinArea"></div>
+        </div>
+      </div>
+    </>
+  );
+}
