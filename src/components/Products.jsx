e312c202c75ec491c8b4812b1186fe26699cc03d
Ben <ben10baek@hotmail.com>
2023-09-14 19:15:42 -0700

    Added sort ability on products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,33 +3,49 @@ import { useEffect, useState } from "react";
 import { specificProducts } from "../api/products";
 export default function Products({ category }) {
   const [products, setProducts] = useState([]);
+  const [sort, setSort] = useState("desc");
   useEffect(() => {
     console.log(category);
+
     async function getProducts() {
-      const encodedString = encodeURIComponent(category);
+      // sort is added with descending as default
+      const encodedString = encodeURIComponent(category) + `?sort=${sort}`;
       console.log(encodedString);
       const getProducts = await specificProducts(encodedString);
       setProducts(getProducts);
     }
     getProducts();
+  }, [category, sort]);
+
+  useEffect(() => {
+    setSort("desc");
   }, [category]);
+
+  async function updateSort(e) {
+    e.preventDefault();
+    if (sort == "desc") {
+      setSort("asc");
+    } else {
+      setSort("desc");
+    }
+  }
+
   console.log(products);
   return (
     <>
       <div className="productsContainer">
         <div className="filterMenu">
-          <div className="filtersContainer">
+          <div className="filterMenuContainer">
             <h2>{category.toUpperCase()}</h2>
-            <div className="filters">
+            <div className="filterContainer">
               <ul>
                 <li>
-                  <a href="#">Filter1</a>
+                  <a href="#" onClick={updateSort}>
+                    Sort: {sort}
+                  </a>
                 </li>
                 <li>
-                  <a href="#">Filter2</a>
-                </li>
-                <li>
-                  <a href="#">Filter3</a>
+                  <a href="#">Price</a>
                 </li>
               </ul>
             </div>
