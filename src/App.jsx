7bffd96b8d4191fde5c704fb7e53f0a9a2d35f62
Ben <ben10baek@hotmail.com>
2023-09-18 19:18:48 -0700

    Added outline for footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from "./components/NavBar";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
@@ -25,9 +25,39 @@ function App() {
               ></Route>
             </Routes>
           </div>
+          <footer>
+            <div>
+              <p>
+                <Link to="/products/men's clothing">Men</Link>
+                <Link to="/products/women's clothing">Women</Link>
+                <Link to="/products/jewlery">Jewlery</Link>
+                <Link to="/products/electronics">Electronics</Link>
+              </p>
+            </div>
+            <div>
+              <p>
+                <a href="#">Contact Us</a>
+                <a href="#ig">
+                  ig
+                  {/* <i className="fa-brands fa-instagram"></i> */}
+                </a>
+                <a href="#fb">
+                  fb
+                  {/* <i className="fa-brands fa-facebook"></i> */}
+                </a>
+                <a href="#x">
+                  x{/* <i className="fa-brands fa-x-twitter"></i> */}
+                </a>
+                <a href="#github">GitHub Repo</a>
+              </p>
+            </div>
+            <div>
+              <p>Fullstack Academy Capstone Project 2023</p>
+              <p>Ben B.</p>
+            </div>
+          </footer>
         </BrowserRouter>
       </div>
-      <footer>Footer</footer>
     </>
   );
 }
