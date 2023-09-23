154297ce5e6965ae8c07be5b5bd943bc9e7aa561
Ben <ben10baek@hotmail.com>
2023-09-23 14:51:36 -0700

    Added subtotal state value

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,19 +3,23 @@ import { getCart } from "../api/cart";
 import { getProduct } from "../api/products";
 export default function Cart() {
   const [cart, setCart] = useState({});
+  const [totalCost, setTotalCost] = useState(0);
   // placeholding cart
   const userId = 5;
   useEffect(() => {
     async function getUserCart(userId) {
       const cartItems = await getCart(userId);
-      console.log(cartItems);
       const cartProducts = cartItems.products;
-      console.log("cartProducts", cartProducts[0]);
+      let total = 0;
       for (let index in cartProducts) {
-        const product = cartProducts[index];
-        const productName = await getProduct(product.productId);
-        product.productName = productName;
+        const productObj = cartProducts[index];
+        const product = await getProduct(productObj.productId);
+        productObj.productInfo = product;
+        const itemTotal = product.price * productObj.quantity;
+        productObj.itemTotal = itemTotal;
+        total = total + itemTotal;
       }
+      setTotalCost(total);
       setCart(cartProducts);
     }
     getUserCart(userId);
@@ -47,6 +51,7 @@ export default function Cart() {
                 <tr>
                   <th>Product</th>
                   <th>Quantity</th>
+                  <th>Cost</th>
                 </tr>
               </thead>
               <tbody>
@@ -55,8 +60,9 @@ export default function Cart() {
                     return (
                       <>
                         <tr key={item.id}>
-                          <td>{item.productName.title}</td>
+                          <td>{item.productInfo.title}</td>
                           <td>{item.quantity}</td>
+                          <td>${item.itemTotal}</td>
                         </tr>
                       </>
                     );
@@ -66,6 +72,7 @@ export default function Cart() {
                     <tr className="noItems">
                       <td>No Products</td>
                       <td>N/A</td>
+                      <td>0</td>
                     </tr>
                   </>
                 )}
@@ -78,7 +85,16 @@ export default function Cart() {
             <h2>Summary</h2>
           </div>
           <div className="costPreview">
-            <div></div>
+            <div>
+              <div>
+                <p>
+                  SubTotal: <b>${totalCost}</b>
+                </p>
+              </div>
+              <div>
+                <p>Total ahhh</p>
+              </div>
+            </div>
           </div>
           <div className="checkoutBtnContainer">
             <button>checkout</button>
